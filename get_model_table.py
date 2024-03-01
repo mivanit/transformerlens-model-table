767d52b30f39153930f88e1d77c3b02ba1a2a72f
mivanit <mivanits@umich.edu>
2024-03-01 02:18:15 -0800

    run format: black, isort, pycln

--- get_model_table.py
+++ get_model_table.py
@@ -1,28 +1,24 @@
-from pathlib import Path
-import warnings
 import multiprocessing
-from multiprocessing import Pool
-from functools import partial
+import warnings
 from copy import deepcopy
+from functools import partial
+from multiprocessing import Pool
+from pathlib import Path
 
 import pandas as pd
-import tqdm
-import yaml
-
 # pytorch
 import torch
-
+import tqdm
 # transformerlens
 import transformer_lens
-from transformer_lens import HookedTransformer, HookedTransformerConfig
-from transformer_lens.loading_from_pretrained import (
-    get_pretrained_model_config
-)
-
-# muutils
-from muutils.misc import shorten_numerical_to_str
+import yaml
 from muutils.dictmagic import condense_tensor_dict
 from muutils.json_serialize import json_serialize
+# muutils
+from muutils.misc import shorten_numerical_to_str
+from transformer_lens import HookedTransformer, HookedTransformerConfig
+from transformer_lens.loading_from_pretrained import \
+    get_pretrained_model_config
 
 # forces everything to meta tensors
 DEVICE: torch.device = torch.device("meta")
@@ -70,27 +66,28 @@ CONFIG_ATTRS_COPY: list[str] = [
     "normalization_type",
 ]
 
+
 def get_model_info(
-        model_name: str, 
-        include_cfg: bool = True,
-        include_tensor_dims: bool = True,
-        tensor_dims_fmt: str = "yaml",
-    ) -> dict:
+    model_name: str,
+    include_cfg: bool = True,
+    include_tensor_dims: bool = True,
+    tensor_dims_fmt: str = "yaml",
+) -> dict:
     """get information about the model from the default alias model name
-    
+
     # Parameters:
-     - `model_name : str`   
+     - `model_name : str`
         the default alias model name
-     - `include_cfg : bool`   
+     - `include_cfg : bool`
         whether to include the model config as a yaml string
        (defaults to `True`)
-     - `include_tensor_dims : bool`   
+     - `include_tensor_dims : bool`
         whether to include the model tensor shapes
        (defaults to `True`)
-     - `tensor_dims_fmt : str`   
+     - `tensor_dims_fmt : str`
         the format of the tensor shapes. one of "yaml", "json", "dict"
        (defaults to `"yaml"`)
-    """    
+    """
     # assumes the input is a default alias
     if model_name not in transformer_lens.loading.DEFAULT_MODEL_ALIASES:
         raise ValueError(f"Model name {model_name} not found in default aliases")
@@ -100,7 +97,9 @@ def get_model_info(
     model_info: dict = {
         "name.default_alias": model_name,
         "name.official": official_name,
-        "name.aliases": list(transformer_lens.loading.MODEL_ALIASES.get(official_name, [])),
+        "name.aliases": list(
+            transformer_lens.loading.MODEL_ALIASES.get(official_name, [])
+        ),
         "model_type": None,
     }
 
@@ -114,7 +113,7 @@ def get_model_info(
             break
 
     # search for model size in name
-    param_count_from_name: str|None = None
+    param_count_from_name: str | None = None
     for part in parts:
         if (
             part[-1].lower() in ["m", "b", "k"]
@@ -125,16 +124,17 @@ def get_model_info(
 
     # update model info from config
     model_cfg: HookedTransformerConfig = get_pretrained_model_config(model_name)
-    model_info.update({
-        "name.from_cfg": model_cfg.model_name,
-        "n_params.as_str": shorten_numerical_to_str(model_cfg.n_params),
-        "n_params.as_int": model_cfg.n_params,
-        "n_params.from_name": param_count_from_name,
-        **{
-            f"config.{attr}": getattr(model_cfg, attr)
-            for attr in CONFIG_ATTRS_COPY
-        },
-    })
+    model_info.update(
+        {
+            "name.from_cfg": model_cfg.model_name,
+            "n_params.as_str": shorten_numerical_to_str(model_cfg.n_params),
+            "n_params.as_int": model_cfg.n_params,
+            "n_params.from_name": param_count_from_name,
+            **{
+                f"config.{attr}": getattr(model_cfg, attr) for attr in CONFIG_ATTRS_COPY
+            },
+        }
+    )
 
     # put the whole config as yaml (for readability)
     if include_cfg:
@@ -152,27 +152,38 @@ def get_model_info(
             model_cfg_copy: HookedTransformerConfig = deepcopy(model_cfg)
             # set device to "meta" -- don't actually initialize the model with real tensors
             model_cfg_copy.device = DEVICE
-            # don't need to download the tokenizer 
+            # don't need to download the tokenizer
             model_cfg_copy.tokenizer_name = None
             # init the fake model
-            model: HookedTransformer = HookedTransformer(model_cfg_copy, move_to_device=True)
+            model: HookedTransformer = HookedTransformer(
+                model_cfg_copy, move_to_device=True
+            )
             # state dict
-            model_info["tensor_shapes.state_dict"] = condense_tensor_dict(model.state_dict(), return_format=tensor_dims_fmt)
-            model_info["tensor_shapes.state_dict.raw__"] = condense_tensor_dict(model.state_dict(), return_format="dict")
+            model_info["tensor_shapes.state_dict"] = condense_tensor_dict(
+                model.state_dict(), return_format=tensor_dims_fmt
+            )
+            model_info["tensor_shapes.state_dict.raw__"] = condense_tensor_dict(
+                model.state_dict(), return_format="dict"
+            )
             # input shape for activations -- "847"~="bat", subtract 7 for the context window to make it unique
             input_shape: tuple[int, int, int] = (847, model_cfg.n_ctx - 7)
             # why? to replace the batch and seq_len dims with "batch" and "seq_len" in the yaml
-            dims_names_map: dict[int, str] = {input_shape[0]: "batch", input_shape[1]: "seq_len"}
+            dims_names_map: dict[int, str] = {
+                input_shape[0]: "batch",
+                input_shape[1]: "seq_len",
+            }
             # run with cache to activation cache
-            _, cache = model.run_with_cache(torch.empty(input_shape, dtype=torch.long, device=DEVICE))
+            _, cache = model.run_with_cache(
+                torch.empty(input_shape, dtype=torch.long, device=DEVICE)
+            )
             # condense using muutils and store
             model_info["tensor_shapes.activation_cache"] = condense_tensor_dict(
-                cache, 
+                cache,
                 return_format=tensor_dims_fmt,
                 dims_names_map=dims_names_map,
             )
             model_info["tensor_shapes.activation_cache.raw__"] = condense_tensor_dict(
-                cache, 
+                cache,
                 return_format="dict",
                 dims_names_map=dims_names_map,
             )
@@ -184,7 +195,7 @@ def get_model_info(
     return model_name, model_info
 
 
-def safe_try_get_model_info(model_name: str, kwargs: dict|None = None) -> dict|None:
+def safe_try_get_model_info(model_name: str, kwargs: dict | None = None) -> dict | None:
     """for parallel processing, to catch exceptions and return None instead of raising them"""
     if kwargs is None:
         kwargs = {}
@@ -196,32 +207,36 @@ def safe_try_get_model_info(model_name: str, kwargs: dict|None = None) -> dict|N
 
 
 def make_model_table(
-        verbose: bool, 
-        allow_except: bool = False,
-        parallelize: bool|int = True,
-        **kwargs,
-    ) -> pd.DataFrame:
+    verbose: bool,
+    allow_except: bool = False,
+    parallelize: bool | int = True,
+    **kwargs,
+) -> pd.DataFrame:
     """make table of all models. kwargs passed to `get_model_info()`"""
     model_names: list[str] = list(transformer_lens.loading.DEFAULT_MODEL_ALIASES)
-    model_data: list[tuple[str, dict|None]] = list()
+    model_data: list[tuple[str, dict | None]] = list()
 
     if parallelize:
         # parallel
-        n_processes: int = parallelize if isinstance(parallelize, int) else multiprocessing.cpu_count()
+        n_processes: int = (
+            parallelize if isinstance(parallelize, int) else multiprocessing.cpu_count()
+        )
         with Pool(processes=multiprocessing.cpu_count()) as pool:
             # Use imap for ordered results, wrapped with tqdm for progress bar
-            imap_results: list[dict|None] = list(tqdm.tqdm(
-                pool.imap(
-                    partial(safe_try_get_model_info, **kwargs),
-                    model_names,
-                ),
-                total=len(model_names),
-                desc="Loading model info",
-                disable=not verbose,
-            ))
-        
+            imap_results: list[dict | None] = list(
+                tqdm.tqdm(
+                    pool.imap(
+                        partial(safe_try_get_model_info, **kwargs),
+                        model_names,
+                    ),
+                    total=len(model_names),
+                    desc="Loading model info",
+                    disable=not verbose,
+                )
+            )
+
         model_data = imap_results
-    
+
     else:
         # serial
         with tqdm.tqdm(
@@ -238,20 +253,29 @@ def make_model_table(
                     model_data.append(None)
 
     # figure out what to do with failed models
-    failed_models: list[str] = [model_name for model_name, result in model_data if result is None]
-    msg: str = f"Failed to get model info for {len(failed_models)}/{len(model_names)} models: {failed_models}"
+    failed_models: list[str] = [
+        model_name for model_name, result in model_data if result is None
+    ]
+    msg: str = (
+        f"Failed to get model info for {len(failed_models)}/{len(model_names)} models: {failed_models}"
+    )
     if not allow_except:
         if failed_models:
             # raise exception if we don't allow exceptions
-            raise ValueError(msg)        
+            raise ValueError(msg)
     else:
         warnings.warn(msg)
 
     # filter out failed models if we allow exceptions
-    model_data_filtered: list[dict] = [result for _, result in model_data if result is not None]
+    model_data_filtered: list[dict] = [
+        result for _, result in model_data if result is not None
+    ]
     return pd.DataFrame(model_data_filtered)
 
-def write_model_table(model_table: pd.DataFrame, path: Path = _MODEL_TABLE_PATH) -> None:
+
+def write_model_table(
+    model_table: pd.DataFrame, path: Path = _MODEL_TABLE_PATH
+) -> None:
     # to jsonlines
     model_table.to_json(path, orient="records", lines=True)
     # to csv
@@ -261,42 +285,47 @@ def write_model_table(model_table: pd.DataFrame, path: Path = _MODEL_TABLE_PATH)
 
 
 def get_model_table(
-        verbose: bool = True, 
-        force_reload: bool = True,
-        do_write: bool = True,
-        parallelize: bool|int = True,
-        **kwargs,
-    ) -> pd.DataFrame:
+    verbose: bool = True,
+    force_reload: bool = True,
+    do_write: bool = True,
+    parallelize: bool | int = True,
+    **kwargs,
+) -> pd.DataFrame:
     """get the model table either by generating or reading from jsonl file
 
     # Parameters:
-     - `verbose : bool`   
+     - `verbose : bool`
         whether to show progress bar
        (defaults to `True`)
-     - `force_reload : bool`   
+     - `force_reload : bool`
         force creating the table from scratch, even if file exists
        (defaults to `True`)
-     - `do_write : bool`   
+     - `do_write : bool`
         whether to write the table to disk, if generating
        (defaults to `True`)
      - `**kwargs`
         eventually passed to `get_model_info()`
-    
+
     # Returns:
-     - `pd.DataFrame` 
+     - `pd.DataFrame`
         the model table. rows are models, columns are model attributes
-    """    
-    
+    """
+
     if not _MODEL_TABLE_PATH.exists() or force_reload:
         # read the table from jsonl
-        model_table: pd.DataFrame = make_model_table(verbose=verbose, parallelize=parallelize, **kwargs)
+        model_table: pd.DataFrame = make_model_table(
+            verbose=verbose, parallelize=parallelize, **kwargs
+        )
         if do_write:
             write_model_table(model_table, _MODEL_TABLE_PATH)
     else:
         # generate it from scratch
-        model_table: pd.DataFrame = pd.read_json(_MODEL_TABLE_PATH, orient="records", lines=True)
+        model_table: pd.DataFrame = pd.read_json(
+            _MODEL_TABLE_PATH, orient="records", lines=True
+        )
 
     return model_table
+
 
 if __name__ == "__main__":
     get_model_table(verbose=True, force_reload=True)
